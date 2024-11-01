d5a09e4d1efb4f6cc318d63905d4a012c3008c6c
JIHANE ELMAJDI <emleyamanda@gmail.com>
2024-11-01 17:28:42 +0100

    fonctionner le boutton save

--- script.js
+++ script.js
@@ -33,7 +33,36 @@ function getFormData() {
     document.querySelector("#Descroption-task").value;
 
   const durationTaskValue = document.querySelector("#Duration-Task").value;
-  const priorityValue = document.querySelector("#priority").value;
-  // console.log(priority);
+  const statusValue = document.querySelector("#status").value;
+
+  const todoColumn = document.querySelector(".tasks1");
+  const doingColumn = document.querySelector(".tasks2");
+  const doneColumn = document.querySelector(".tasks3");
+
+  const task = `<div class="tasks1">
+          <div class="exemple1">
+            <div>
+              <h4>${taskNameValue}</h4>
+              <p>${durationTaskValue}</p>
+              <p>${statusValue}</p>
+              <p>${descriptionTaskValue}</p>
+            </div>
+            <div class="icons">
+              <button type="button" class="delete">
+                <i class="fa-solid fa-trash"></i>
+              </button>
+            </div>
+          </div>`;
+
+  if (statusValue === "to-do") {
+    todoColumn.insertAdjacentHTML("beforeend", task);
+  }
+
+  if (statusValue === "doing") {
+    doingColumn.insertAdjacentHTML("beforeend", task);
+  }
+  if (statusValue === "done") {
+    doneColumn.insertAdjacentHTML("beforeend", task);
+  }
 }
-getFormData();
+// getFormData();
